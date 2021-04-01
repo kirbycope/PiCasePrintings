0e996b8eb6b9bbc31107fe4a4bc4509b2670b8c5
Tim Cope <kirbycope@gmail.com>
2021-03-31 22:27:12 -0700

    Update oled.py

--- oled.py
+++ oled.py
@@ -55,3 +55,78 @@ disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 # Alternatively you can specify an explicit I2C bus number, for example
 # with the 128x32 display you would use:
 # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+
+# 128x32 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# 128x64 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# Alternatively you can specify a software SPI implementation by providing
+# digital GPIO pin numbers for all the required display pins.  For example
+# on a Raspberry Pi with the 128x32 display you might use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# The font to be displayed
+font = ImageFont.truetype('type_writer.ttf', 8)
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d ' ' -f1"
+    IP = subprocess.check_output(cmd, shell = True )
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell = True )
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2}'"
+    MemUsage = subprocess.check_output(cmd, shell = True )
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell = True )
+    cmd = "vcgencmd measure_temp | cut -f 2 -d '='"
+    temp = subprocess.check_output(cmd, shell = True )
+
+    # Write four lines of text.
+
+    draw.text((x, top+2), "IP: " + str(IP,'utf-8'), font=font, fill=255)
+    draw.text((x, top+10), str(CPU,'utf-8') + "        " "temp: " + str(temp,'utf-8') , font=font, fill=255)
+    draw.text((x, top+18), str(MemUsage,'utf-8'), font=font, fill=255)
+    draw.text((x, top+27), str(Disk,'utf-8'), font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(.1)
