c7b933e66c181fd7b7570611e576a51f747e143e
Tim Cope <kirbycope@gmail.com>
2021-04-01 21:57:11 -0700

    Update oled.py

--- oled.py
+++ oled.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2017 Adafruit Industries
+# Author: Tony DiCola & James DeVito
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 import time
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
@@ -43,28 +63,33 @@ bottom = height-padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-# The font to be displayed
-font = ImageFont.truetype('type_writer.ttf', 8)
+# Load default font.
+font = ImageFont.load_default()
 
 while True:
+
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
+
     # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d ' ' -f1"
     IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU:%.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2}'"
+    cmd = "free -m | awk 'NR==2{printf \"Mem:%s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
     MemUsage = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk:%d/%dGB %s\", $3,$2,$5}'"
     Disk = subprocess.check_output(cmd, shell = True )
-    cmd = "vcgencmd measure_temp | cut -f 2 -d '='"
+    cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
     temp = subprocess.check_output(cmd, shell = True )
-    # Write four lines of text.
-    draw.text((x, top+2), "IP: " + str(IP,'utf-8'), font=font, fill=255)
-    draw.text((x, top+10), str(CPU,'utf-8') + "        " "temp: " + str(temp,'utf-8') , font=font, fill=255)
-    draw.text((x, top+18), str(MemUsage,'utf-8'), font=font, fill=255)
-    draw.text((x, top+27), str(Disk,'utf-8'), font=font, fill=255)
+
+    # Write two lines of text.
+
+    draw.text((x, top), "IP:" + str(IP,'utf-8'), font=font, fill=255)
+    draw.text((x, top+8), str(CPU,'utf-8') + " " "TEMP:" + str(temp,'utf-8') , font=font, fill=255)
+    draw.text((x, top+16), str(MemUsage,'utf-8'), font=font, fill=255)
+    draw.text((x, top+25), str(Disk,'utf-8'), font=font, fill=255)
+
     # Display image.
     disp.image(image)
     disp.display()
